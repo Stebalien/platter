48ee71bc218783b56e67c54a731a758fe3704f86
Steven Allen <steven@stebalien.com>
2014-01-07 11:41:23 -0800

    Cleaner EventHandler

--- platter/event.py
+++ platter/event.py
@@ -1,17 +1,19 @@
 class EventHandler:
+    __slots__ = ("__callbacks",)
+
     def __init__(self):
-        self.callbacks = {}
+        self.__callbacks = {}
 
     def trigger(self, signal, *args, **kwargs):
-        for cb in self.callbacks.get(signal, ()):
+        for cb in self.__callbacks.get(signal, ()):
             cb(*args, **kwargs)
-        for cb in self.callbacks.get("all", ()):
+        for cb in self.__callbacks.get("all", ()):
             cb(signal, *args, **kwargs)
 
     def on(self, signal, cb):
-        self.callbacks.setdefault(signal, set()).add(cb)
+        self.__callbacks.setdefault(signal, set()).add(cb)
 
     def off(self, signal, cb):
-        if signal in self.callbacks:
-            self.callbacks[signal].remove(cb)
+        if signal in self.__callbacks:
+            self.__callbacks[signal].remove(cb)
 
