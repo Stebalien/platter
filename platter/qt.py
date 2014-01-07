d53b1dfe098e22503644728885208a479c2a173a
Steven Allen <steven@stebalien.com>
2014-01-07 11:41:44 -0800

    "Significant changes"
    * Better listener pattern.
    * Multi-file support.
    * Drag and drop.

--- platter/qt.py
+++ platter/qt.py
@@ -3,47 +3,74 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 from .util import async
 from .server import PlatterHTTPServer
 from cgi import escape
+from urllib.request import pathname2url
 import os
 
+class Signaler(QtCore.QObject):
+    signal = QtCore.pyqtSignal(tuple, dict)
+
+    def __init__(self, fn, block=False):
+        super().__init__()
+
+        self.function = fn
+        if block:
+            self.signal.connect(self.__handler, QtCore.Qt.BlockingQueuedConnection)
+        else:
+            self.signal.connect(self.__handler)
+
+    def __handler(self, args, kwargs):
+        self.function(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        self.signal.emit(args, kwargs)
+
+def sync(fn, block=False):
+    return Signaler(fn, block)
 
 class IllegalArgumentException(ValueError):
     pass
+
+def path2url(path):
+    return 'file://' + pathname2url(os.path.abspath(path))
 
 class PlatterQt(QtWidgets.QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        try:
-            fname = self.arguments()[1]
-        except:
-            raise IllegalArgumentException("Missing Filename")
+        #try:
+        #    fname = self.arguments()[1]
+        #except:
+        #    raise IllegalArgumentException("Missing Filename")
 
-        if not os.path.exists(fname):
-            raise IllegalArgumentException("File Not Found: %s" % fname)
+        #if not os.path.exists(fname):
+        #    raise IllegalArgumentException("File Not Found: %s" % fname)
 
-        self.transfers = {}
 
         self.server = PlatterHTTPServer()
-
-        self.file = self.server.serve(fname)
-
-        self.main = PlatterQtUI(self.file)
-        self.main.show()
+        self.main = PlatterQtUI()
         self.connectSignals()
+
+        for fpath in self.arguments()[1:]:
+            self.addFile(fpath)
+
+        self.main.show()
 
         self.server_thread = threading.Thread(target=self.server.serve_forever)
         self.server_thread.daemon = True
         self.server_thread.start()
 
     def connectSignals(self):
-        self.file.on("progress", self.proxyProgress)
-        self.file.on("failure", self.proxyFailure)
-        self.file.on("success", self.proxySuccess)
-        self.file.on("start", self.proxyStart)
         self.aboutToQuit.connect(self.onQuit)
 
     def onQuit(self):
         self._shutdown()
+
+    def addFile(self, fpath):
+        if os.path.exists(fpath):
+            self.server.serve(fpath)
+            return True
+        else:
+            return False
 
     def _shutdown(self):
         self.server.shutdown()
@@ -54,71 +81,122 @@ class PlatterQt(QtWidgets.QApplication):
         self._shutdown()
         self.quit()
 
-    def proxyStart(self, transfer):
-        self.main.addTransferSignal.emit(transfer)
-
-    def proxyProgress(self, transfer, progress):
-        try:
-            pane = self.transfers[transfer.client_address[0]]
-        except KeyError:
-            return
-        pane.progressSignal.emit(progress)
-
-    def proxySuccess(self, transfer):
-        try:
-            pane = self.transfers[transfer.client_address[0]]
-        except KeyError:
-            return
-        pane.successSignal.emit()
-
-    def proxyFailure(self, transfer):
-        try:
-            pane = self.transfers[transfer.client_address[0]]
-        except KeyError:
-            return
-        pane.failureSignal.emit()
-
 class PlatterQtUI(QtWidgets.QWidget):
-    addTransferSignal = QtCore.pyqtSignal(object)
-    quitSignal = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.app = QtWidgets.QApplication.instance()
+        QtGui.QIcon.setThemeName('Faenza')
+        self.file_panes = {}
+
+        self.initUI()
+        self.connectSignals()
+
+    def connectSignals(self):
+        self.app.server.on("add", sync(self.onAddFile))
+        self.app.server.on("remove", sync(self.onRemoveFile))
+
+    def dragEnterEvent(self, event):
+        if all(url.isLocalFile() for url in event.mimeData().urls()):
+            event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        for url in event.mimeData().urls():
+            self.app.addFile(url.path())
+
+    def initUI(self):
+        self.setAcceptDrops(True);
+        self.setWindowFlags(QtCore.Qt.Dialog)
+        frame = self.frameGeometry()
+        frame.moveCenter(self.app.desktop().availableGeometry().center())
+        self.move(frame.topLeft())
+        
+        # Build pane
+        self.content = QtWidgets.QWidget()
+        content_layout = QtWidgets.QVBoxLayout()
+        content_layout.setContentsMargins(0,0,0,0)
+        content_layout.addLayout(self.makeFilesPane())
+        content_layout.addStretch(1)
+        content_layout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        self.content.setLayout(content_layout)
+        self.content.hide()
+
+        self.notice = QtWidgets.QLabel("Drag to serve files...")
+        self.notice.setMinimumSize(200, 150)
+        self.notice.setAlignment(QtCore.Qt.AlignCenter)
+
+        window_layout = QtWidgets.QHBoxLayout(self)
+        window_layout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        window_layout.addWidget(self.content)
+        window_layout.addWidget(self.notice)
+
+        self.setLayout(window_layout)
+        self.setWindowTitle('Platter')
+
+    def makeFilesPane(self):
+        files_container = QtWidgets.QWidget()
+        self.files_pane = QtWidgets.QVBoxLayout()
+        self.files_pane.setSpacing(0)
+        self.files_pane.setContentsMargins(0,0,0,0)
+        self.files_pane.addStretch(1)
+
+        return self.files_pane
+
+    def onAddFile(self, f):
+        file_pane = FilePane(f)
+        self.file_panes[f] = file_pane
+        self.files_pane.addWidget(file_pane)
+        self.content.show()
+        self.notice.hide()
+
+    def onRemoveFile(self, f):
+        file_pane = self.file_panes.pop(f)
+        self.files_pane.removeWidget(file_pane)
+        v = file_pane.layout().takeAt(0)
+        while v:
+            w = v.widget()
+            if w:
+                w.hide()
+            v = file_pane.layout().takeAt(0)
+
+        if not self.file_panes:
+            self.notice.show()
+            self.content.hide()
+
+    def closeEvent(self, e):
+        e.ignore()
+        self.app.shutdown()
+        self.notice.setText("Shutting down...")
+        self.content.hide()
+        self.notice.show()
+
+    def keyPressEvent(self, e):
+        if e.key() in (QtCore.Qt.Key_Escape, QtCore.Qt.Key_Q):
+            self.close()
+
+class FilePane(QtWidgets.QWidget):
 
     def __init__(self, f):
         super().__init__()
         self.app = QtWidgets.QApplication.instance()
-        QtGui.QIcon.setThemeName('Faenza')
-
+        self.transfer_panes = {}
         self.file = f
 
         self.initUI()
         self.connectSignals()
 
     def connectSignals(self):
-        self.addTransferSignal.connect(self.onAddTransfer, QtCore.Qt.BlockingQueuedConnection)
-        #self.quitSignal.connect(self.onQuit)
+        self.file.on("add", sync(self.onAddTransfer, True))
+        self.file.on("remove", sync(self.onRemoveTransfer))
 
     def initUI(self):
-        #self.setGeometry(300, 300, 250, 150)
-        self.setWindowFlags(QtCore.Qt.Dialog)
-        frame = self.frameGeometry()
-        frame.moveCenter(self.app.desktop().availableGeometry().center())
-        self.move(frame.topLeft())
-
-        
-        # Build pane
-        self.content = QtWidgets.QWidget()
         content_layout = QtWidgets.QHBoxLayout()
-        content_layout.setContentsMargins(0,0,0,0)
+        content_layout.setContentsMargins(5,5,5,5)
         content_layout.addLayout(self.makeQRCode())
         content_layout.addLayout(self.makeInfoPane())
         content_layout.addStretch(1)
         content_layout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
-        self.content.setLayout(content_layout)
-
-        window_layout = QtWidgets.QHBoxLayout(self)
-        window_layout.addWidget(self.content)
-
-        self.setLayout(window_layout)
-        self.setWindowTitle('Platter')
+        self.setLayout(content_layout)
 
     def makeQRCode(self):
         image = qrcode.make(self.file.url, box_size=5)
@@ -130,15 +208,35 @@ class PlatterQtUI(QtWidgets.QWidget):
         container.addStretch(1)
         return container
 
-    def makeInfoPane(self):
-        title = QtWidgets.QLabel('<h1 style="font-weight: normal;">{filename}</h1>'.format(
-            filename=escape(self.file.basename),
-        ))
+    def makeHeaderPane(self):
+        title = QtWidgets.QLabel(
+            '<a style="color: {color};" href="{url}"><h1 style="font-weight: normal;">{filename}</h1></a>'.format(
+                filename=escape(self.file.basename),
+                url=path2url(self.file.filepath),
+                color=self.palette().brightText().color().name()
+            )
+        )
+        title.setOpenExternalLinks(True)
 
+        removeBtn = QtWidgets.QToolButton()
+        removeBtn.setText('Remove')
+        removeBtn.setIcon(QtGui.QIcon.fromTheme("edit-delete"))
+        removeBtn.clicked.connect(self.onRemove)
+
+        header = QtWidgets.QHBoxLayout()
+        header.addWidget(title)
+        header.addWidget(removeBtn)
+
+        return header
+
+    def onRemove(self):
+        self.file.stop()
+
+    def makeInfoPane(self):
         pane = QtWidgets.QVBoxLayout()
-        pane.addWidget(title)
+        pane.addLayout(self.makeHeaderPane())
         pane.addLayout(self.makeUrlPane())
-        pane.addWidget(self.makeTransferPane())
+        pane.addWidget(self.makeTransfersPane())
         return pane
 
     def makeUrlPane(self):
@@ -154,20 +252,20 @@ class PlatterQtUI(QtWidgets.QWidget):
         url_pane.addStretch(1)
         return url_pane
 
-    def makeTransferPane(self):
-        transfer_container = QtWidgets.QWidget()
-        self.transfer_pane = QtWidgets.QVBoxLayout()
-        self.transfer_pane.setSpacing(0)
-        self.transfer_pane.setContentsMargins(0,0,0,0)
-        self.transfer_pane.addStretch(1)
+    def makeTransfersPane(self):
+        transfers_container = QtWidgets.QWidget()
+        self.transfers_pane = QtWidgets.QVBoxLayout()
+        self.transfers_pane.setSpacing(0)
+        self.transfers_pane.setContentsMargins(0,0,0,0)
+        self.transfers_pane.addStretch(1)
 
         outer_layout = QtWidgets.QVBoxLayout()
-        outer_layout.addLayout(self.transfer_pane)
+        outer_layout.addLayout(self.transfers_pane)
         outer_layout.addStretch(1)
-        transfer_container.setLayout(outer_layout)
+        transfers_container.setLayout(outer_layout)
 
         scroll_container = QtWidgets.QScrollArea()
-        scroll_container.setWidget(transfer_container)
+        scroll_container.setWidget(transfers_container)
         scroll_container.setWidgetResizable(True)
         return scroll_container
 
@@ -175,42 +273,24 @@ class PlatterQtUI(QtWidgets.QWidget):
         self.app.clipboard().setText(self.file.url)
 
     def onAddTransfer(self, request):
-        address = str(request.client_address[0])
-        # Assume that devices only download once (to work around chromes double get)
-        if address in self.app.transfers:
-            transfer = self.app.transfers[address]
-        else:
-            transfer = TransferPane(address)
-            self.app.transfers[address] = transfer
-            self.transfer_pane.addWidget(transfer)
-        transfer.addRequest(request)
-        transfer.onStart()
+        transfer = TransferPane(request)
+        self.transfer_panes[request] = transfer
+        self.transfers_pane.addWidget(transfer)
 
-    def insertTrans(self, widget):
-        self.transfer_pane.addWidget(widget)
+    def onRemoveTransfer(self, request):
+        transfer = self.transfer_panes.pop(request)
+        self.transfers_pane.removeWidget(transfer)
+        v = transfer.layout().takeAt(0)
+        while v:
+            v.widget().hide()
+            v = transfer.layout().takeAt(0)
 
-    def closeEvent(self, e):
-        self.app.shutdown()
-        e.ignore()
-        self.layout().takeAt(0).widget().hide()
-        label = QtWidgets.QLabel("Shutting down...")
-        label.setAlignment(QtCore.Qt.AlignCenter)
-        self.layout().addWidget(label)
-
-
-    def keyPressEvent(self, e):
-        if e.key() in (QtCore.Qt.Key_Escape, QtCore.Qt.Key_Q):
-            self.close()
 
 class TransferPane(QtWidgets.QWidget):
-    progressSignal = QtCore.pyqtSignal(int)
-    successSignal = QtCore.pyqtSignal()
-    failureSignal = QtCore.pyqtSignal()
-    def __init__(self, address):
+    def __init__(self, request):
         super().__init__()
         self.app = QtWidgets.QApplication.instance()
-        self.address = address
-        self.requests = []
+        self.request = request
 
         self.initUI()
         self.connectSignals()
@@ -218,9 +298,12 @@ class TransferPane(QtWidgets.QWidget):
     def initUI(self):
         self.progress_bar = QtWidgets.QProgressBar()
 
+        self.progress_bar.setFormat("{}:{} - %p%".format(*self.request.client_address))
+        self.progress_bar.setValue(self.request.progress)
+        self.progress_bar.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+
         self.close_button = QtWidgets.QToolButton()
         self.close_button.setIcon(QtGui.QIcon.fromTheme("window-close"))
-        self.close_button.clicked.connect(self.remove)
 
 
         layout = QtWidgets.QHBoxLayout()
@@ -228,39 +311,26 @@ class TransferPane(QtWidgets.QWidget):
         layout.addWidget(self.close_button)
         self.setLayout(layout)
 
+
     def connectSignals(self):
-        self.progressSignal.connect(self.onProgress)
-        self.successSignal.connect(self.onSucceed)
-        self.failureSignal.connect(self.onFailure)
+        self.request.on("progress", sync(self.onProgress))
+        self.request.on("success", sync(self.onSucceed))
+        self.request.on("fail", sync(self.onFailure))
+
+        self.close_button.clicked.connect(self.onCancel)
 
     def onProgress(self, progress):
         self.progress_bar.setValue(progress)
 
-    def onStart(self):
-        self.progress_bar.setFormat("{address} - %p%".format(address=self.address))
-        self.progress_bar.setValue(0)
-
-    def addRequest(self, request):
-        self.requests.append(request)
-
     def onSucceed(self):
         self.progress_bar.setValue(100)
-        self.progress_bar.setFormat("{address} - Completed".format(address=self.address))
+        self.progress_bar.setFormat("{}:{} - Completed".format(*self.request.client_address))
 
     def onFailure(self):
-        self.progress_bar.setFormat("{address} - Failed".format(address=self.address))
+        self.progress_bar.setFormat("{}:{} - Failed".format(*self.request.client_address))
     
-    def remove(self):
-        for request in self.requests:
-            request.cancel()
-
-        self.app.main.transfer_pane.removeWidget(self)
-        del self.app.transfers[self.address]
-        v = self.layout().takeAt(0)
-        while v:
-            v.widget().hide()
-            v = self.layout().takeAt(0)
-
+    def onCancel(self):
+        self.request.cancel()
 
 def main():
     import sys
--- platter/server.py
+++ platter/server.py
@@ -9,27 +9,34 @@ from .util import make_code
 
 BUF_SIZE = 16*1024
 
-class PlatterHTTPRequestHandler(BaseHTTPRequestHandler):
+class PlatterHTTPRequestHandler(BaseHTTPRequestHandler, EventHandler):
     canceled = False
+    progress = 0
+    def __init__(self, *args, **kwargs):
+        EventHandler.__init__(self)
+        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
     def do_GET(self):
         try:
-            f = self.server.paths[self.path]
+            self.file = self.server.paths[self.path]
         except:
             self.send_error(404)
             return
-
+        
         try:
-            f.trigger("start", self)
+            self.file._register_request(self)
             self.send_response(200)
+
+            self.trigger("start")
+
             self.send_header("Content-Type", "application/octet-stream")
-            self.send_header("Content-Disposition", "attachment;filename=%s" % quote(f.basename))
-            self.send_header('Content-Length', f.size)
+            self.send_header("Content-Disposition", "attachment;filename=%s" % quote(self.file.basename))
+            self.send_header('Content-Length', self.file.size)
             self.end_headers()
 
-            percent_increment = float(BUF_SIZE)/float(f.size)*100
-            percent_complete = 0
 
-            with open(f.filepath, 'rb') as fobj:
+            percent_increment = float(BUF_SIZE)/float(self.file.size)*100
+
+            with open(self.file.filepath, 'rb') as fobj:
                 while True:
                     buf = fobj.read(BUF_SIZE)
                     if not buf:
@@ -37,12 +44,14 @@ class PlatterHTTPRequestHandler(BaseHTTPRequestHandler):
                     if self.canceled:
                         return
                     self.wfile.write(buf)
-                    percent_complete += percent_increment
-                    f.trigger("progress", self, percent_complete)
+                    self.progress += percent_increment
+                    self.trigger("progress", self.progress)
         except:
-            f.trigger("failure", self)
+            self.trigger("failure")
         else:
-            f.trigger("success", self)
+            self.progress = 100
+            self.trigger("success")
+
 
     def cancel(self):
         self.canceled = True
@@ -56,12 +65,14 @@ class PlatterHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.rfile.close()
             except:
                 pass
+        self.file._unregister_request(self)
 
 class File(EventHandler):
     def __init__(self, server, filepath):
         super().__init__()
 
         self.server = server
+        self.requests = set()
 
         self.filepath = filepath
         self.basename = os.path.basename(filepath)
@@ -76,10 +87,28 @@ class File(EventHandler):
         return "http://%s:%d%s" % (
             self.server.my_ip, self.server.server_port, self.path
         )
+    
+    def stop(self):
+        self.server.unserve(self.filepath)
 
-class PlatterHTTPServer(ThreadingMixIn, HTTPServer):
+        for req in frozenset(self.requests):
+            req.cancel()
+
+
+    def _register_request(self, request):
+        # Assume that devices only download once (to work around chromes double get)
+        self.requests.add(request)
+        self.trigger("add", request)
+
+    def _unregister_request(self, request):
+        self.requests.remove(request)
+        self.trigger("remove", request)
+
+
+class PlatterHTTPServer(ThreadingMixIn, HTTPServer, EventHandler):
 
     def __init__(self, address=("", 0), handler=PlatterHTTPRequestHandler, **kwargs):
+        EventHandler.__init__(self)
         HTTPServer.__init__(self, address, handler, **kwargs)
         self.paths = {}
         self.files = {}
@@ -87,6 +116,18 @@ class PlatterHTTPServer(ThreadingMixIn, HTTPServer):
 
     def serve(self, fpath):
         if fpath in self.files:
-            return self.files[fpath]
+            f = self.files[fpath]
         else:
-            return File(self, fpath)
+            f = File(self, fpath)
+            self.files[f.filepath] = f
+            self.paths[f.path] = f
+
+        self.trigger("add", f)
+        return f
+    
+    def unserve(self, fpath):
+        if fpath in self.files:
+            f = self.files.pop(fpath)
+            del self.paths[f.path]
+            self.trigger("remove", f)
+
