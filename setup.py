2eb65db778742845d64f8973d231c11c38fc5fe8
Steven Allen <steven@stebalien.com>
2014-01-18 16:00:34 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "platter",
-    version = "1.1",
+    version = "1.2",
     packages = find_packages(),
     install_requires = ["Pillow>=2.3.0", "qrcode", "netifaces"],
     author = "Steven Allen",
